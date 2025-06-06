4bafd6fee7cee8c18ef522418a75c97c4e77d287
Rushiv99 <rushiprajapati895@gmail.com>
2025-06-06 15:58:56 +0000

    datatypes complete

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+console.log("Rushiv")
+
+// code readability should be high
+
+let name = "Rushiv" // string
+let age = 20 // number
+let isLoggedin = false //boolean datatype
+let state; // undefine not null
+
+//datatypes
+//number => 2 to power 53
+//bigint
+//boolean => false/true
+//string => ""
+//null => standalone  value
+//undefined => not the asign the value
+//symbol => unique
+
+//object
+
+console.log( typeof "rushiv")
+console.log( typeof state)
+console.log( typeof 30)
+console.log( typeof true)
+console.log( typeof null)
