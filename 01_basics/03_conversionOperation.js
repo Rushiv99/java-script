c6a39b97384aee88c217e1cf0d1eec81497de165
Rushiv99 <rushiprajapati895@gmail.com>
2025-06-06 16:21:12 +0000

    datatype conversion is done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+"use strict"
+let score = "23"
+console.log(typeof score)
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber)
+let id = "23abc"
+let idinnumber = Number(id)
+console.log(id)
+console.log(typeof idinnumber)
+console.log(idinnumber)
+
+console.log("------------------------------------------------")
+let id1 = true
+let id1innumber = Number(id1)
+console.log(id1)
+console.log(typeof id1innumber)
+console.log(id1innumber)
+
+// we also use Boolean , String for convert the type of data
