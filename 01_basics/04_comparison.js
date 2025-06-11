9d545810d4d26c03282185fe30ca7f6908363769
Rushiv99 <rushiprajapati895@gmail.com>
2025-06-11 01:09:08 +0000

    work done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2<=3)
+// console.log(2==3)
+// console.log(2!=3)
+
+console.log("2"==2)  // true
+console.log("2">1)    // true
+console.log("3"!= 4)  //true
+console.log(4 >"3")   // true
+
+console.log("----------------------------")
+
+console.log(null == 0)   //false
+console.log(null != 0)   //true
+console.log(null >= 0)   //true
+console.log(null <= 0)   //true
+
+console.log("-----------------------------")
+
+
+console.log(undefined == 0)    //false 
+console.log(undefined != 0)    //true
+console.log(undefined >= 0)   //false
+console.log(undefined <= 0)    //false
+
+console.log("-------------------------------")
+
+// === strict check
+
+console.log("2" === 2) // false
