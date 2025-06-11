9689d447ce934673329d331dc48248c71f024ea6
Rushiv99 <rushiprajapati895@gmail.com>
2025-06-11 00:45:48 +0000

    work complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,34 @@ console.log(typeof id1innumber)
 console.log(id1innumber)
 
 // we also use Boolean , String for convert the type of data
+
+console.log("---------------------------------------------------")
+let str1 = "hello"
+let str2 = " Rushi"
+let str3 = str1+str2
+console.log(str3)            // hello Rushi
+console.log(str2+str1)       //  Rushihello
+
+console.log("------------------------------------------------------")
+
+console.log("1"+2)
+console.log(1+ "2")
+console.log(1+ "2"+1)
+console.log("1"+ 2+2)
+console.log(1+ 2+"2")
+
+console.log("------------------------------------------------------")
+
+console.log(+true)
+console.log(+"")
+
+console.log("-------------------------------------------------------")
+
+let gameCounter = 100
+let g1 = gameCounter++
+let g2 = ++gameCounter
+
+console.log(g1)
+console.log(g2)
+
+console.log("-------------------------------------------------------")
